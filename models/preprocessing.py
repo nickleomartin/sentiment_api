0c3080f1a38d96998ef9d2f63adfdb5dfa63e3af
Nicholas Leo Martin <nickleomartin@gmail.com>
2018-06-13 15:28:48 +0200

    Completed VocabularyBuilder and DocIdTransformer in preprocessing.py. Removed dataset_wrapper.py.

--- models/preprocessing.py
+++ models/preprocessing.py
@@ -1,6 +1,184 @@
 import re
 import numpy as np 
-from sklearn.base
+import nltk
+from collections import Counter
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.externals import joblib
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+
+from models import utils 
+
+
+## TODO: Use gensim instead for vocabulary creation
+## TODO: Context-sensitive spelling correction
+## TODO: More comprehensive punctuation remove
+
+class VocabularyBuilder(object):
+	""" 
+	Converts vocabulary to word and char ids
+	
+	Example:
+	--------
+	from models.preprocessing import VocabularyBuilder
+	
+	documents = ["This is a sentence. Additional one.", "This is another sentence!"]
+	dw = VocabularyBuilder()
+	dw.build(documents)
+	"""
+	def __init__(self, max_vocab_size=10000, lowercase=True, unk_token=True, specials=('<pad>',),
+					 training=True):
+		self._max_vocab_size = max_vocab_size
+		self._lowercase = lowercase
+		self._unk_token = unk_token
+		self._token2id = {token: i for i, token in enumerate(specials)}
+		self._id2token = list(specials)
+		self._training = training
+		self._word_count = Counter()
+		self._char_count = Counter()
+
+	def process_document(self, document):
+		""" Sentence tokenization of document """
+		return nltk.sent_tokenize(document)
+
+	def process_sentence(self, sentence):
+		""" Process sentence string and store word ids"""
+		if self._lowercase:
+			words = nltk.word_tokenize(sentence.lower())
+		else:
+			words = nltk.word_tokenize(sentence)
+
+		## Update word counter if training 
+		if self._training:
+			self._word_count.update(words)
+		return words
+
+	def process_word(self, word): 
+		""" Process words and store character ids """
+		## Update character counter if training
+		if self._training:
+			self._char_count.update([l for l in word])
+
+	def construct_vocab_and_reverse_vocab(self):
+		""" Construct token-to-id dict and id-to-token list """
+		idx = len(self._token2id)
+		for word in self._word_count.keys():
+			self._token2id[word] = idx
+			self._id2token.append(word)
+			idx += 1
+
+		## Add characters to token2id
+		for char in self._char_count.keys():
+			if char not in self._token2id:
+				self._token2id[char] = idx
+				self._id2token.append(char)
+				idx += 1
+
+		## Add unknown token to end of vocabulary
+		if self._unk_token:
+			unk = '<unk>'
+			self._token2id[unk] = idx
+			self._id2token.append(unk)
+			idx += 1
+
+	def build(self, X):
+		""" Obtain word and char ids """
+		for document in X:
+			words = self.process_sentence(document)
+			for word in words:
+				self.process_word(word)
+		self.construct_vocab_and_reverse_vocab()
+
+		## Indicate training is complete
+		self._training = False
+	
+	def token_to_id(self, token):
+		""" Return index of token """
+		return self._token2id.get(token, len(self._token2id)-1)
+
+	def id_to_token(self, idx):
+		""" Return token given inedx """
+		return self._id2token[idx]
+
+	def document_to_word_ids(self, document):
+		""" Convert single unseen document to word ids """
+		words = self.process_sentence(document)
+		## If None then return unk_token
+		doc_ids = [self.token_to_id(word) for word in words]
+		return doc_ids
+
+	def document_to_char_ids(self, document):
+		""" Convert single unseen document to character ids """
+		doc_ids = [self.token_to_id(l) for l in document]
+		return doc_ids
+
+	@property
+	def vocabulary(self):
+		""" Access vocabulary """
+		return self._token2id
+
+	@property
+	def reverse_vocabulary(self):
+		""" Access reverse vocabulary """
+		return self._id2token
+
+
+
+class DocIdTransformer(BaseEstimator, TransformerMixin):
+	""" 
+	Convert documents to document-id matrix 
+	
+	Example:
+	--------
+	from models.preprocessing import DocIdTransformer
+	
+	X = ['This is a document', 'This is another document!', "And this a third..."]
+	doc_id_transformer = DocIdTransformer()
+	X_word_feat, X_char_feat, doc_lengths = doc_id_transformer.fit_transform(X)
+	"""
+	def __init__(self, lower=True, normalize_num=True, user_char=True, initial_vocab=None):
+		self._normalize_num = normalize_num
+		self._use_char = user_char
+		self._vocab_builder = VocabularyBuilder(lowercase=lower)
+
+	def fit(self, X):
+		""" Get vocabulary from documents """
+		## TODO: Validate input type and shape of X
+		
+		## Build vocabulary
+		self._vocab_builder.build(X)
+		return self
+
+	def transform(self, X):
+		""" Convert documents to document ids """
+		w_ids = [self._vocab_builder.document_to_word_ids(doc) for doc in X]
+		doc_lengths = np.array([len(doc) for doc in X], dtype='int32')
+		w_ids = pad_sequences(w_ids, padding='post')
+
+		if self._use_char:
+			ch_ids = [[self._vocab_builder.document_to_char_ids(w) for w in doc] for doc in X]
+			ch_ids = utils.pad_nested_sequence(ch_ids)
+			features = [w_ids, ch_ids, doc_lengths]
+		else:
+			features = [w_ids, doc_lengths]
+		return features
+
+	def fit_transform(self, X):
+		""" Return document-id matrix given documents """
+		return self.fit(X).transform(X)
+
+	def save(self, file_path):
+		""" Save object to file_path """
+		joblib.dump(self, file_path)
+
+	@classmethod
+	def load(cls, file_path):
+		""" Load saved DocIdTransformer from file_path """
+		doc_id_transformer = joblib.load(file_path)
+		return doc_id_transformer 
+
+
+
 
 
 
