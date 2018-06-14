69f46716d87567b26ad7c0d8ac2f8763a2e4222e
Nicholas Leo Martin <nickleomartin@gmail.com>
2018-06-14 10:17:38 +0200

    Added exception handling to API

--- api/views.py
+++ api/views.py
@@ -16,7 +16,6 @@ def load_model():
 	params_file = "trained_models/2018_06_13_23_params"
 	preprocessor_file = "trained_models/2018_06_13_23_preprocessor" 
 	sentiment_model = SentimentAnalysisModel.load(weights_file, params_file, preprocessor_file)
-
 	graph = tf.get_default_graph()
 	return sentiment_model, graph
 
@@ -28,22 +27,24 @@ class PredictSentimentView(View):
 
 	def get(self, request, *args, **kwargs):
 		""" GET requests returns a warning message"""
-		return JsonResponse({"response":"Use the post method","status":400 })
+		return JsonResponse({"response":"Use the post method","status": 400})
 
 	def post(self, request, *args, **kwargs):
 		""" POST requests take in text and return sentiment"""
-		# text = self.request.GET['data']
-		json_object = json.loads(request.body.decode("utf-8"))
-		text = json_object['input']
+		try:
+			json_object = json.loads(request.body.decode("utf-8"))
+			text = json_object['input']
 
-		## Inference on loaded model
-		global graph
-		with graph.as_default():
-			sent_class = MODEL.predict([text])
+			## Inference on loaded model
+			global graph
+			with graph.as_default():
+				sent_class = MODEL.predict([text])
 
-		## TODO: Asynchronous Celery call to process model prediction? Not needed now. 
+			## TODO: Asynchronous Celery call to process model prediction? Not needed now. 
 
-		return JsonResponse({"text": text, "sentiment_score": sent_class, "response": "Successful", "status": 200})
+			return JsonResponse({"text": text, "sentiment_score": sent_class, "response": "Successful", "status": 200})
+		except:
+			return JsonResponse({"text": text, "sentiment_score": "Error", "response": "Successful", "status": 400})
 
 	@method_decorator(csrf_exempt)
 	def dispatch(self, request, *args, **kwargs):
